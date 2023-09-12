16b8fa816c7d1bdbdf9b83413bd5cae184e68faa
Camille Phiquepal <camille.phiquepal@gmail.com>
2023-09-12 13:34:14 +0200

    Add zero vel task

---
+++ libs/GeometricUtilityTasks/axis_alignment.cpp
@@ -0,0 +1,141 @@
+/*  ------------------------------------------------------------------
+    Copyright 2016 Camille Phiquepal
+    email: camille.phiquepal@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or (at
+    your option) any later version. This program is distributed without
+    any warranty. See the GNU General Public License for more details.
+    You should have received a COPYING file of the full GNU General Public
+    License along with this program. If not, see
+    <http://www.gnu.org/licenses/>
+    --------------------------------------------------------------  */
+
+#include <axis_alignment.h>
+
+//-----AxisAlignment----------------//
+
+using namespace std;
+
+void AxisAlignment::phi( arr& y, arr& J, const rai::KinematicWorld& G )
+{
+  arr tmp_y = zeros( 1 );
+  arr tmp_J = zeros( 1, G.q.N );
+
+  const auto body = G.getFrameByName( bobyName_ );
+
+  arr bodyAxisDirection, bodyJAxisDirection;
+  G.kinematicsVec( bodyAxisDirection, bodyJAxisDirection, body, axis_ );
+
+  const double dot_product = dot( bodyAxisDirection, axis_ );
+
+  const double cost = 1 - dot_product;
+
+  tmp_y( 0 ) = cost;
+  tmp_J.setMatrixBlock( - ( ~ axis_ ) * bodyJAxisDirection, 0, 0 );
+
+  // commit results
+  y = tmp_y;
+  if(&J) J = tmp_J;
+}
+
+//-----AxisOrthogonal----------------//
+
+void AxisOrthogonal::phi( arr& y, arr& J, const rai::KinematicWorld& G )
+{
+  arr tmp_y = zeros( 1 );
+  arr tmp_J = zeros( 1, G.q.N );
+
+  const auto body = G.getFrameByName( bobyName_ );
+
+  arr bodyAxisDirection, bodyJAxisDirection;
+  G.kinematicsVec( bodyAxisDirection, bodyJAxisDirection, body, bodyAxis_ );
+
+  const double dot_product = dot( bodyAxisDirection, worldAxis_ );
+
+  tmp_y( 0 ) = dot_product;
+  tmp_J.setMatrixBlock( ( ~ worldAxis_ ) * bodyJAxisDirection, 0, 0 );
+
+  // commit results
+  y = tmp_y;
+  if(&J) J = tmp_J;
+}
+
+//-----BoxAxisAligned----------------//
+
+void BoxAxisAligned::phi( arr& y, arr& J, const rai::KinematicWorld& G )
+{
+  arr tmp_y = zeros( 1 );
+  arr tmp_J = zeros( 1, G.q.N );
+
+  // effector
+  const auto effector = G.getFrameByName( effectorName_ );
+
+  arr effectorAxisDirection, effectorJAxisDirection;
+  G.kinematicsVec( effectorAxisDirection, effectorJAxisDirection, effector, effectorAxis_ );
+
+  // box
+  const auto box = G.getFrameByName( boxName_ );
+
+  arr boxAxisDirection, boxJAxisDirection;
+  G.kinematicsVec( boxAxisDirection, boxJAxisDirection, box, boxAxis_ );
+
+  const double dot_product = dot( boxAxisDirection, effectorAxisDirection );
+
+  tmp_y( 0 ) = 1.0 - dot_product;
+  tmp_J.setMatrixBlock( - ( ~ boxAxisDirection ) * effectorJAxisDirection - ( ~effectorAxisDirection ) * boxJAxisDirection, 0, 0 );
+
+  // commit results
+  y = tmp_y;
+  if(&J) J = tmp_J;
+}
+
+//-----TargetPosition----------------//
+
+void TargetPosition::phi( arr& y, arr& J, const rai::KinematicWorld& G )
+{
+  arr tmp_y = zeros( 3 );
+  arr tmp_J = zeros( 3, G.q.N );
+
+  // effector
+  const auto effector = G.getFrameByName( effectorName_ );
+
+  arr effectorPosition, effectorJPosition;
+  G.kinematicsPos( effectorPosition, effectorJPosition, effector );
+
+  // box
+  const auto box = G.getFrameByName( boxName_ );
+
+  arr targetPosition, jTargetPosition;
+  G.kinematicsPos( targetPosition, jTargetPosition, box, targetPosition_ );
+
+  tmp_y = targetPosition - effectorPosition;
+  tmp_J.setMatrixBlock( jTargetPosition - effectorJPosition, 0, 0 );
+
+  // commit results
+  y = tmp_y;
+  if(&J) J = tmp_J;
+}
+
+
+//-----ZeroVelocity----------------//
+
+void ZeroVelocity::phi( arr& y, arr& J, const rai::KinematicWorld& G )
+{
+  arr tmp_y = zeros( 3 );
+  arr tmp_J = zeros( 3, G.q.N );
+
+  // effector
+  const auto object = G.getFrameByName( objectName_ );
+
+  arr objectVelocity, objectJVelocity;
+  G.kinematicsPos( objectVelocity, objectJVelocity, object );
+
+  tmp_y = objectVelocity;
+  tmp_J.setMatrixBlock( objectJVelocity, 0, 0 );
+
+  // commit results
+  y = tmp_y;
+  if(&J) J = tmp_J;
+}
---
+++ libs/GeometricUtilityTasks/axis_alignment.h
@@ -0,0 +1,131 @@
+/*  ------------------------------------------------------------------
+    Copyright 2016 Camille Phiquepal
+    email: camille.phiquepal@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or (at
+    your option) any later version. This program is distributed without
+    any warranty. See the GNU General Public License for more details.
+    You should have received a COPYING file of the full GNU General Public
+    License along with this program. If not, see
+    <http://www.gnu.org/licenses/>
+    --------------------------------------------------------------  */
+
+#pragma once
+
+#include <math_utility.h>
+
+#include <Kin/feature.h>
+#include <Kin/taskMaps.h>
+
+// alignment to one world axis
+struct AxisAlignment:Feature
+{
+  AxisAlignment( const char* bobyName, const arr & axis )
+    : bobyName_     ( bobyName )
+    , axis_( axis )
+  {
+
+  }
+
+  virtual void phi( arr& y, arr& J, const rai::KinematicWorld& G );
+
+  uint dim_phi( const rai::KinematicWorld& G ) { return 1; }
+
+  rai::String shortTag(const rai::KinematicWorld& G){ return STRING("AxisAlignment"); }
+
+  private:
+    const rai::String bobyName_;
+    const arr axis_;
+};
+
+// orthogonal to one world axis
+struct AxisOrthogonal:Feature
+{
+  AxisOrthogonal( const char* bobyName, const arr & bodyAxis, const arr & worldAxis )
+    : bobyName_     ( bobyName )
+    , bodyAxis_( bodyAxis )
+    , worldAxis_( worldAxis )
+  {
+
+  }
+
+  virtual void phi( arr& y, arr& J, const rai::KinematicWorld& G );
+
+  uint dim_phi( const rai::KinematicWorld& G ) { return 1; }
+
+  rai::String shortTag(const rai::KinematicWorld& G ){ return STRING("AxisOrthogonal"); }
+
+  private:
+    const rai::String bobyName_;
+    const arr bodyAxis_; // typically effector
+    const arr worldAxis_;
+};
+
+// aligned to axis of box
+struct BoxAxisAligned:Feature
+{
+  BoxAxisAligned( const char* effectorName, const arr & effectorAxis, const char * boxName, const arr & boxAxis )
+    : effectorName_( effectorName )
+    , effectorAxis_( effectorAxis )
+    , boxName_( boxName )
+    , boxAxis_( boxAxis )
+  {
+
+  }
+
+  virtual void phi( arr& y, arr& J, const rai::KinematicWorld& G );
+
+  uint dim_phi( const rai::KinematicWorld& G ) { return 1; }
+
+  rai::String shortTag(const rai::KinematicWorld& G ){ return STRING("BoxAxisAligned"); }
+
+  private:
+    const rai::String effectorName_;
+    const arr effectorAxis_;
+    const rai::String boxName_;
+    const arr boxAxis_;
+};
+
+// Position target position
+struct TargetPosition:Feature
+{
+  TargetPosition( const char* effectorName, const char * boxName, const arr & targetPosition )
+    : effectorName_( effectorName )
+    , boxName_( boxName )
+    , targetPosition_( targetPosition )
+  {
+
+  }
+
+  virtual void phi( arr& y, arr& J, const rai::KinematicWorld& G );
+
+  uint dim_phi( const rai::KinematicWorld& G ) { return 3; }
+
+  rai::String shortTag(const rai::KinematicWorld& G ){ return STRING("TargetPosition"); }
+
+  private:
+    const rai::String effectorName_;
+    const rai::String boxName_;
+    const arr targetPosition_; // in box's frame
+};
+
+// Zero velocity (to be used in 1st order to force a zero velocity)
+struct ZeroVelocity:Feature
+{
+    ZeroVelocity( const char* objectName )
+      : objectName_( objectName )
+    {
+
+    }
+
+    virtual void phi( arr& y, arr& J, const rai::KinematicWorld& G );
+
+    uint dim_phi( const rai::KinematicWorld& G ) { return 3; }
+
+    rai::String shortTag(const rai::KinematicWorld& G ){ return STRING("ZeroVelocity"); }
+
+    private:
+      const rai::String objectName_;
+  };
