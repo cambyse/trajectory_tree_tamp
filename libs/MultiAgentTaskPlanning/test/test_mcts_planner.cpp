ddd1735d74e6bd367235cb4665d530c64a43df4f
Camille Phiquepal <camille.phiquepal@gmail.com>
2024-03-03 22:53:34 +0100

    unit test for policy improvement

--- libs/MultiAgentTaskPlanning/test/test_mcts_planner.cpp
+++ libs/MultiAgentTaskPlanning/test/test_mcts_planner.cpp
@@ -50,6 +50,8 @@ TEST_F(MCTSPlannerTest, MCTS_WhenBuildingMCTSDecisionGraph_AndMakingAnEdgeWithIn
 
   tp.setFol( "LGP-2-blocks-1-side-fol.g" );
 
+  /// SIMULATE TAMP WITH ACTION INFEASIBLE
+
   // Step 1
   tp.solve();
   const auto policy = tp.getPolicy();
@@ -62,7 +64,7 @@ TEST_F(MCTSPlannerTest, MCTS_WhenBuildingMCTSDecisionGraph_AndMakingAnEdgeWithIn
   EXPECT_EQ( policy.leaves().back()->id(), 5 );
 
   // simulate intergation of a policy
-  tp.getRewards().set( 0, policy.root()->children().front()->id() , -1000.0);
+  tp.getRewards().set( 0, policy.root()->children().front()->data().decisionGraphNodeId, -1000.0);
 
   // Step 2
   tp.solve();
@@ -75,10 +77,84 @@ TEST_F(MCTSPlannerTest, MCTS_WhenBuildingMCTSDecisionGraph_AndMakingAnEdgeWithIn
   EXPECT_EQ( policy_1.leaves().size(), 2 );
   EXPECT_EQ( policy_1.leaves().front()->id(), 3 );
   EXPECT_EQ( policy_1.leaves().back()->id(), 5 );
-
-  // check that policy is changed
   EXPECT_NE( policy.leaves().front()->data().decisionGraphNodeId, policy_1.leaves().front()->data().decisionGraphNodeId );
   EXPECT_NE( policy.leaves().back()->data().decisionGraphNodeId, policy_1.leaves().back()->data().decisionGraphNodeId );
+  EXPECT_GE( policy.value(), policy_1.value() );
+
+  // simulate intergation of a policy
+  tp.getRewards().set( 0, policy_1.root()->children().front()->data().decisionGraphNodeId, -1000.0 );
+
+  // Step 3
+  tp.solve();
+
+  const auto policy_2 = tp.getPolicy();
+
+  tp.saveMCTSGraphToFile( "decision_graph_mcts_2.gv" );
+  savePolicyToFile( policy_2, "-mcts" );
+
+  EXPECT_EQ( policy_2.leaves().size(), 2 );
+  EXPECT_NE( policy_1.leaves().front()->data().decisionGraphNodeId, policy_2.leaves().front()->data().decisionGraphNodeId );
+  EXPECT_NE( policy_1.leaves().back()->data().decisionGraphNodeId, policy_2.leaves().back()->data().decisionGraphNodeId );
+  EXPECT_GE( policy_1.value(), policy_2.value() );
+}
+
+TEST_F(MCTSPlannerTest, MCTS_WhenBuildingMCTSDecisionGraph_AndMakingAnEdgeLessCostlyThanR0_ExpectSamePolicyWithHigherValue)
+{
+  tp.setR0( -1.0 );
+  tp.setNIterMinMax( 1000, 1000000 );
+  tp.setRollOutMaxSteps( 50 );
+  tp.setNumberRollOutPerSimulation( 1 );
+  tp.setExplorationTerm( 15 ); // 20
+  tp.setVerbose( false );
+
+  tp.setFol( "LGP-2-blocks-1-side-fol.g" );
+
+  /// SIMULATE TAMP WITH ACTION INFEASIBLE
+
+  // Step 1
+  tp.solve();
+  const auto policy = tp.getPolicy();
+
+  tp.saveMCTSGraphToFile( "decision_graph_mcts.gv" );
+  savePolicyToFile( policy, "-mcts" );
+
+  EXPECT_EQ( policy.leaves().size(), 2 );
+  EXPECT_EQ( policy.leaves().front()->id(), 3 );
+  EXPECT_EQ( policy.leaves().back()->id(), 5 );
+
+  // simulate intergation of a policy
+  tp.getRewards().set( 0, policy.root()->children().front()->data().decisionGraphNodeId, -0.5 );
+
+  // Step 2
+  tp.solve();
+
+  const auto policy_1 = tp.getPolicy();
+
+  tp.saveMCTSGraphToFile( "decision_graph_mcts_1_bis.gv" );
+  savePolicyToFile( policy_1, "-mcts" );
+
+  EXPECT_EQ( policy_1.leaves().size(), 2 );
+  EXPECT_EQ( policy_1.leaves().front()->id(), 3 );
+  EXPECT_EQ( policy_1.leaves().back()->id(), 5 );
+  EXPECT_EQ( policy.leaves().front()->data().decisionGraphNodeId, policy_1.leaves().front()->data().decisionGraphNodeId );
+  EXPECT_EQ( policy.leaves().back()->data().decisionGraphNodeId, policy_1.leaves().back()->data().decisionGraphNodeId );
+  EXPECT_LT( policy.value(), policy_1.value() );
+
+  // simulate intergation of a policy
+  tp.getRewards().set( 0, policy_1.root()->children().front()->data().decisionGraphNodeId, -0.25 );
+
+  // Step 3
+  tp.solve();
+
+  const auto policy_2 = tp.getPolicy();
+
+  tp.saveMCTSGraphToFile( "decision_graph_mcts_2_bis.gv" );
+  savePolicyToFile( policy_2, "-mcts" );
+
+  EXPECT_EQ( policy_2.leaves().size(), 2 );
+  EXPECT_EQ( policy_1.leaves().front()->data().decisionGraphNodeId, policy_2.leaves().front()->data().decisionGraphNodeId );
+  EXPECT_EQ( policy_1.leaves().back()->data().decisionGraphNodeId, policy_2.leaves().back()->data().decisionGraphNodeId );
+  EXPECT_LT( policy_1.value(), policy_2.value() );
 }
 //
 int main(int argc, char **argv)
